59fdc6313bf476edac9508667af9e8af1533aa2a
DaviDSramos22 <estudosdr22@gmail.com>
2025-05-31 15:58:34 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('busca').addEventListener('keyup', function () {
-    const termo = this.value.toLowerCase();
-    const tutoriais = document.querySelectorAll('#lista-tutoriais li');
-
-    tutoriais.forEach(tutorial => {
-        const texto = tutorial.textContent.toLowerCase();
-        tutorial.style.display = texto.includes(termo) ? '' : 'none';
-    });
-});
